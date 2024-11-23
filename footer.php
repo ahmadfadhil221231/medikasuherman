39e09b2eb4c58de118969dc4139512eb142b9857
ahmadfadhil221231 <ahmadfadhil221231@gmail.com>
2024-11-23 15:12:59 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,74 +1,23 @@
-<?php
-/**
- * The template for displaying the footer
- *
- * Contains the closing of the #content div and all content after.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package Updated News
- */
-
+<?php 
+$newsmash_hs_other_story 		= get_theme_mod( 'newsmash_hs_other_story', '1');
+if($newsmash_hs_other_story=='1'):
+get_template_part('template-parts/section','other-story'); 
+endif; 
 ?>
-
-</div>
-
-<?php if ( ! is_front_page() || is_home() ) { ?>
-</div>
-</div><!-- #content -->
-
-<?php
-}
-
-if ( is_front_page() ) {
-
-	require get_template_directory() . '/inc/frontpage-sections/above-footer-widgets-area.php';
-
-}
-
-?>
-
-<footer id="colophon" class="site-footer">
-	<?php if ( is_active_sidebar( 'footer-1' ) || is_active_sidebar( 'footer-2' ) || is_active_sidebar( 'footer-3' ) || is_active_sidebar( 'footer-4' ) ) : ?>
-	<div class="top-footer">
-		<div class="theme-wrapper">
-			<div class="top-footer-widgets">
-
-				<?php for ( $i = 1; $i <= 4; $i++ ) { ?>
-					<div class="footer-widget">
-						<?php dynamic_sidebar( 'footer-' . $i ); ?>
-					</div>
-				<?php } ?>
-
-			</div>
-		</div>
+</div></div>
+<footer class="dt_footer footer-dark">
+	<div class="dt-container-md">
+		<?php 
+			// Footer Widget
+			do_action('newsmash_footer_widget');
+			
+			// Footer Copyright
+			do_action('newsmash_footer_bottom'); 
+		?>
 	</div>
-	<?php
-endif;
-$updated_news_search = array( '[the-year]', '[site-link]' );
-$replace             = array( date( 'Y' ), '<a href="' . esc_url( home_url( '/' ) ) . '">' . esc_attr( get_bloginfo( 'name', 'display' ) ) . '</a>' );
-$copyright_default   = sprintf( esc_html_x( 'Copyright &copy; %1$s %2$s', '1: Year, 2: Site Title with home URL', 'updated-news' ), '[the-year]', '[site-link]' );
-$copyright_text      = get_theme_mod( 'updated_news_copyright_txt', $copyright_default );
-$copyright_text      = str_replace( $updated_news_search, $replace, $copyright_text );
-?>
-<div class="bottom-footer">
-	<div class="theme-wrapper">
-		<div class="bottom-footer-info">
-			<div class="site-info">
-				<span>
-					<?php echo wp_kses_post( $copyright_text ); ?>
-					<?php echo sprintf( esc_html__( 'Theme: %1$s By %2$s.', 'updated-news' ), wp_get_theme()->get( 'Name' ), '<a href="' . wp_get_theme()->get( 'AuthorURI' ) . '">' . wp_get_theme()->get( 'Author' ) . '</a>' ); ?>
-				</span>	
-			</div><!-- .site-info -->
-		</div>
-	</div>
-</div>
-
-</footer><!-- #colophon -->
-<a href="#" id="scroll-to-top" class="updated-news-scroll-to-top"><i class="fas fa-chevron-up"></i></a>
-</div><!-- #page -->
-
-<?php wp_footer(); ?>
+</footer>
+<?php 
+wp_footer(); ?>
 </body>
  <div style="display: none;">
 <a href="https://pafisukarame.org/">https://pafisukarame.org/</a>
