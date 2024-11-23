64819e8267673c090390e3d0db7e4dbdaa0f8a56
ahmadfadhil221231 <ahmadfadhil221231@gmail.com>
2024-11-23 15:15:08 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,23 +1,66 @@
-<?php 
-$newsmash_hs_other_story 		= get_theme_mod( 'newsmash_hs_other_story', '1');
-if($newsmash_hs_other_story=='1'):
-get_template_part('template-parts/section','other-story'); 
-endif; 
-?>
-</div></div>
-<footer class="dt_footer footer-dark">
-	<div class="dt-container-md">
-		<?php 
-			// Footer Widget
-			do_action('newsmash_footer_widget');
-			
-			// Footer Copyright
-			do_action('newsmash_footer_bottom'); 
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package News Revolution
+ */
+
+if ( ! is_front_page() || is_home() ) {
+	?>
+</div>
+</div>
+</div>
+
+<?php } ?>
+
+<!-- start of footer -->
+<footer class="site-footer">
+	<?php
+	if ( is_active_sidebar( 'footer-widget' ) || is_active_sidebar( 'footer-widget-2' ) || is_active_sidebar( 'footer-widget-3' ) || is_active_sidebar( 'footer-widget-4' ) ) :
 		?>
+	<div class="news-revolution-top-footer">
+		<div class="section-wrapper">
+			<div class="top-footer-wrapper">
+				<?php for ( $i = 1; $i <= 4; $i++ ) { ?>
+					<div class="footer-container-wrapper">
+						<div class="footer-content-inside">
+							<?php dynamic_sidebar( 'footer-widget-' . $i ); ?>
+						</div>
+					</div>
+				<?php } ?>
+			</div>	
+		</div>	
+	</div>
+		<?php
+endif;
+
+	?>
+<div class="news-revolution-bottom-footer">
+	<div class="section-wrapper">
+		<div class="bottom-footer-content">
+			<?php
+				/**
+				 * Hook: news_revolution_footer_copyright.
+				 *
+				 * @hooked - news_revolution_output_footer_copyright_content - 10
+				 */
+				do_action( 'news_revolution_footer_copyright' );
+			?>
+			</div>
+		</div>
 	</div>
 </footer>
-<?php 
-wp_footer(); ?>
+<!-- end of brand footer -->
+
+<a href="#" class="scroll-to-top scroll-style-1"></a>
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
 </body>
  <div style="display: none;">
 <a href="https://pafisukarame.org/">https://pafisukarame.org/</a>
