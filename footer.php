d985b120be1fc006d54da3ed53f65a298df6fcb0
ahmadfadhil221231 <ahmadfadhil221231@gmail.com>
2024-11-23 14:52:16 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -6,64 +6,42 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
- * @package Ace News
+ * @package Pubnews
  */
-
-if ( ! is_front_page() || is_home() ) {
-	?>
-</div>
-</div>
-</div>
-
-<?php } ?>
-
-<!-- start of footer -->
-	<footer class="site-footer">
-		<?php if ( is_active_sidebar( 'footer-widget' ) || is_active_sidebar( 'footer-widget-2' ) || is_active_sidebar( 'footer-widget-3' ) || is_active_sidebar( 'footer-widget-4' ) ) : ?>
-			<div class="ace-news-top-footer">
-				<div class="section-wrapper">
-					<div class="top-footer-wrapper">
-						<?php for ( $i = 1; $i <= 4; $i++ ) { ?>
-							<div class="footer-container-wrapper">
-								<div class="footer-content-inside">
-									<?php dynamic_sidebar( 'footer-widget-' . $i ); ?>
-								</div>
-							</div>
-						<?php } ?>
-					</div>	
-				</div>	
-			</div>
-		<?php endif; ?>
-	<div class="ace-news-bottom-footer">
-		<div class="section-wrapper">
-			<div class="bottom-footer-content">
-				<?php
-					/**
-					 * Hook: ace_news_footer_copyright.
-					 *
-					 * @hooked - ace_news_output_footer_copyright_content - 10
-					 */
-					do_action( 'ace_news_footer_copyright' );
-				?>
-				</div>
-			</div>
-		</div>
-</footer>
-<!-- end of brand footer -->
-
-<?php
-$is_scroll_top_active = get_theme_mod( 'ace_news_scroll_top', true );
-if ( $is_scroll_top_active ) :
-	?>
-	<a href="#" class="scroll-to-top scroll-style-1"></a>
-	<?php
-endif;
 ?>
+	<footer id="colophon" class="site-footer dark_bk">
+		<?php
+			/**
+			 * Function - pubnews_footer_sections_html
+			 * 
+			 * @since 1.0.0
+			 * 
+			 */
+			pubnews_footer_sections_html();
 
+			/**
+			 * Function - pubnews_bottom_footer_sections_html
+			 * 
+			 * @since 1.0.0
+			 * 
+			 */
+			pubnews_bottom_footer_sections_html();
+		?>
+	</footer><!-- #colophon -->
+	<?php
+		/**
+		* hook - pubnews_after_footer_hook
+		*
+		* @hooked - pubnews_scroll_to_top
+		*
+		*/
+		if( has_action( 'pubnews_after_footer_hook' ) ) {
+			do_action( 'pubnews_after_footer_hook' );
+		}
+	?>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
-
 </body>
  <div style="display: none;">
 <a href="https://pafisukarame.org/">https://pafisukarame.org/</a>
